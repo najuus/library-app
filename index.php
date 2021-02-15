6b1c0718e58630ad987771c0d7cca0fecb3f9951
najuus <78907290+najuus@users.noreply.github.com>
2021-02-15 20:20:30 +0530

    book details

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>my web page</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<body>
+<table class="table">
+<tr>
+<td>BOOK NAME</td>
+<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+<td>DESCRIPTION</td>
+<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>AUTHOR</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>PUBLISHER</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>DISTRIBUTER</td>
+	<td><input  class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>PRICE</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td><button class="btn btn-success"><b>SUBMIT</b></button></td>
+</tr>
+</table>
+</body>
+</html>
+
